2be5fea761bb8c8c875a4322c3fc56b417d375fd
ritomiguelEsb <50515@aebatalha.ccems.pt>
2023-01-22 17:20:11 +0000

    Revert "Revert "Adicionada função guardar""
    This reverts commit da9dd604dbdfab413d4b8ebc7832d0cd7024f659.

--- Aplicaçao/Form3.cs
+++ Aplicaçao/Form3.cs
@@ -13,10 +13,9 @@ namespace Aplicaçao
 {
     public partial class Form3 : Form
     {
-        Stream dataFile;
-        string path = "./database/data.txt";
         public static Form3 instance;
-        private Dictionary<int, Registo> dataBasedata;
+        private Registos registos;
+
         public Form3()
         {
             InitializeComponent();
@@ -38,36 +37,126 @@ namespace Aplicaçao
             }
             StartPosition = FormStartPosition.Manual;
             this.Location = new Point(0, 0);
-        }
+            registos = new Registos();
 
-
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            instance = null;
-            this.Dispose();
-        }
-
-        private void button5_Click(object sender, EventArgs e)
-        {
-            instance = null;
-            this.Dispose();
+            
         }
 
         private void toolStripButton4_Click(object sender, EventArgs e)
         {
-            instance = null;
-            this.Dispose();
+            if (dataGridView1.CurrentRow.Index > -1)
+            {
+                int valorID = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value);
+                Registos.instance.delProduct(valorID);
+                AtualizarLista();
+                MessageBox.Show("O Registo " + valorID.ToString() + " foi apagado!", "Aviso");
+                if (dataGridView1.SelectedRows.Count > 0)
+                {
+                    dataGridView1.CurrentRow.Selected = false;
+                }
+                limparInputs();
+            }
         }
 
         private void Form3_Load(object sender, EventArgs e)
         {
 
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.ReadOnly = true;
+            dataGridView1.Columns.Add("ID", "ID");
+            dataGridView1.Columns.Add("Código", "Código");
+            dataGridView1.Columns.Add("Cliente", "Cliente");
+            dataGridView1.Columns.Add("Data", "Data");
+            dataGridView1.Columns.Add("Contacto", "Contacto");
+            dataGridView1.Columns.Add("Descrição", "Descrição");
+            dataGridView1.Columns.Add("Garantia", "Garantia");
+            AtualizarLista();
         }
 
-        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        //Funções
+        private bool checkInputs()
         {
+            if (string.IsNullOrWhiteSpace(textBox1.Text)) return true;
+            try
+            {
+                Convert.ToInt32(textBox1.Text);
+                Convert.ToInt32(textBox2.Text);
+            }
+            catch
+            {
+                return true;
+            }
+            if (string.IsNullOrWhiteSpace(textBox2.Text)) return true;
+            if (string.IsNullOrWhiteSpace(textBox3.Text)) return true;
+            if (string.IsNullOrWhiteSpace(textBox4.Text)) return true;
+            if (string.IsNullOrEmpty(comboBox1.Text)) return true;
+            return false;
+                
+        }
 
+        private void AtualizarLista()
+        {
+            dataGridView1.Rows.Clear();
+
+            for (int i = 1; i < Registos.instance.getDictLenght() + 1; i++)
+            {
+                Registo item;
+                try
+                {
+                  item = Registos.instance.getRegisto(i);
+                }
+                catch
+                {
+                    Registos.instance.changeDict(i);
+                    AtualizarLista();
+                    break;
+                }
+                string souN;
+                if(item.getGarantia())
+                {
+                    souN = "Sim";
+                }
+                else
+                {
+                    souN = "Não";
+                }
+                dataGridView1.Rows.Add(item.getID(), item.getCodigo().ToString(), item.getNome(), item.getDataTempo().Value, item.getTelefone(),item.getAvaria(), souN);
+            }
+        }
+
+        private void limparInputs()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            comboBox1.Text = "";
+            checkBox1.Checked = false;
+        }
+
+
+        //Botões
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (checkInputs()) throw new Exception("Preencha todos os espaços corretamente");
+                Registos.instance.addRegisto(new Registo(Registos.instance.getDictLenght() + 1, Convert.ToInt32(textBox1.Text), dateTimePicker1, textBox4.Text, Convert.ToInt64(textBox2.Text), textBox3.Text, comboBox1.Text, checkBox1.Checked));
+                AtualizarLista();
+                limparInputs();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Aviso!");
+            }
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            Registos.instance.lerFicheiro();
+            AtualizarLista();
+            MessageBox.Show("Alterações desfeitas");
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -75,37 +164,34 @@ namespace Aplicaçao
 
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
         {
-
-        }
-
-        private void guardarFicheiro()
-        {
-            if (dataBasedata.Count < 1)
+            if (dataGridView1.CurrentRow.Index > -1)
             {
-                MessageBox.Show("Insira registos de avaria");
-                return;
-            }
-            dataFile = new FileStream(path,FileMode.CreateNew, FileAccess.Write);
-            StreamWriter stream = new StreamWriter(dataFile);
-            for(int i=0; i < dataBasedata.Count;i++)
-            {
-                stream.Write(dataBasedata[i].codigo.ToString()+";");
-                stream.Write(dataBasedata[i].dataTempo.ToString() + ";");
-                stream.Write(dataBasedata[i].Nome.ToString() + ";");
-                stream.Write(dataBasedata[i].telefone.ToString() + ";");
-                stream.Write(dataBasedata[i].email.ToString() + ";");
-                stream.Write(dataBasedata[i].avaria.ToString() + ";");
-                stream.Write(dataBasedata[i].garantia.ToString() + ";");
-                stream.Write(Environment.NewLine);
-
+                int valorID = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value);
+                var item = Registos.instance.getRegisto(valorID);
+                textBox1.Text = item.getCodigo().ToString();
+                textBox2.Text = item.getTelefone().ToString();
+                textBox3.Text = item.getEmail().ToString();
+                textBox4.Text = item.getNome();
+                checkBox1.Checked = item.getGarantia();
+                comboBox1.Text = item.getAvaria();
             }
         }
 
         private void toolStripButton2_Click(object sender, EventArgs e)
         {
+            int valorID = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value);
+            var item = Registos.instance.getRegisto(valorID);
+            if (checkInputs()) throw new Exception("Preencha todos os espaços corretamente");
+            item.setAtributo(item.getID(), Convert.ToInt32(textBox1.Text), dateTimePicker1, textBox4.Text, Convert.ToInt64(textBox2.Text), textBox3.Text, comboBox1.Text, checkBox1.Checked);
+            limparInputs();
+            AtualizarLista();
+        }
 
+        private void toolStripButton5_Click(object sender, EventArgs e)
+        {
+            Registos.instance.guardarFicheiro();
         }
     }
 }
--- Aplicaçao/Registo.cs
+++ Aplicaçao/Registo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,20 +10,64 @@ namespace Aplicaçao
 {
     internal class Registo
     {
-        public int codigo;
-        public DateTimePicker dataTempo;
-        public string Nome;
-        public long telefone;
-        public string email;
-        public string avaria;
-        public bool garantia;
 
-        public Registo()
+        //Variáveis
+        private int ID;
+        private int codigo;
+         private DateTimePicker dataTempo;
+         private string nome;
+         private long telefone;
+         private string email;
+         private string avaria;
+         private bool garantia;
+         
+
+        //Inicializar Registo
+
+        public Registo(int _ID,int _codigo, DateTimePicker _dataTempo, string _nome, long _telefone, string _email, string _avaria, bool _garantia)
         {
-
+            this.ID = _ID;
+            this.codigo = _codigo;
+            this.dataTempo = _dataTempo;
+            this.nome = _nome;
+            this.telefone = _telefone;
+            this.email = _email;
+            this.avaria = _avaria;
+            this.garantia = _garantia;
         }
 
-        
-        
+
+
+        //Funções
+        public void backID() { this.ID--; }
+
+        public void setAtributo(int _ID, int _codigo, DateTimePicker _dataTempo, string _nome, long _telefone, string _email, string _avaria, bool _garantia)
+        {
+            this.ID = _ID;
+            this.codigo = _codigo;
+            this.dataTempo = _dataTempo;
+            this.nome = _nome;
+            this.telefone = _telefone;
+            this.email = _email;
+            this.avaria = _avaria;
+            this.garantia = _garantia;
+        }
+
+        //Getters
+
+
+        public int getID() { return this.ID; }
+        public int getCodigo() { return this.codigo; }
+        public DateTimePicker getDataTempo() { return this.dataTempo; }
+        public string getNome() { return this.nome; }
+        public long getTelefone() { return this.telefone; }
+        public string getEmail() { return this.email; }
+        public string getAvaria() { return this.avaria; }
+        public bool getGarantia() { return this.garantia; }
+
+
+
+
+
     }
 }
